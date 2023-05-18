c45b36224b0266e5eccedb0e0cba9ad0d73be006
Federico Vancini <fevancin@gmail.com>
2023-05-18 23:54:53 +0200

    Added full input generation

--- README.md
+++ README.md
@@ -5,18 +5,25 @@ Generate new instances with `generate_instances.py`:
 - option `-n` select the number of instance to generate (default `1`)
 - option `-p` accepts the prefix name for each instance folder (default `instance`)
 - option `-o` specify the output directory for the instances (default `instances`)
+- option `--only-requests` doesn't generate the protocol information 
 - option `-v` to see the output in verbose format
+
+---
 
 Compute the subsumptions on the days with `compute_subsumptions.py`:
 - option `-m` specify the method used (`asp` or `milp`, default `asp`)
 - option `-i` specify the instances input directory (default `instances`)
 - option `-v` to see the output in verbose format
 
+---
+
 Solve all subproblems with `solve_subproblems.py`:
 - option `-m` specify the method used (`asp`, `milp_basic`, `milp_optimized` or `milp_epsilon`, default `asp`)
 - option `-i` specify the instances input directory (default `instances`)
 - option `-v` to see the output in verbose format
 
-Compute all cores with `compute_cores.py`:
+---
+
+Compute all unsatisfiable cores with `compute_cores.py`:
 - option `-i` specify the instances input directory (default `instances`)
 - option `-v` to see the output in verbose format
--- generate_intances.py
+++ generate_intances.py
@@ -3,6 +3,7 @@ import sys
 import json
 import random
 import argparse
+from datetime import datetime
 
 ################################################################################
 #                            GENERATOR CONFIGURATION                           #
@@ -27,6 +28,24 @@ patient_priority = (1, 4)
 
 request_amount = (1, 4)         # patient number in the same day
 request_size = (1, 2)           # numer of packets requested by a single patient
+
+interdiction_probability = 0.25
+interdiction_duration = (1, 2)
+
+necessity_probability = 0.25
+necessity_size = (1, 2)
+necessity_start = (1, 3)
+necessity_duration = (1, 7)
+
+patient_protocol = (1, 2)
+protocol_frequency = (2, 7)
+protocol_tolerance = (0, 2)
+protocol_start = (1, 10)
+protocol_duration = (10, 30)
+
+iteration_size = (1, 3)
+iteration_number = (1, 2)
+iteration_shift = (-10, 10)
 
 ################################################################################
 
@@ -93,6 +112,119 @@ def generate_requests():
             }
     return requests
 
+def generate_timestamp():
+    timestamp = datetime.now().strftime("%a-%d-%b-%Y-%H-%M-%S")
+    return timestamp
+
+def generate_total_care_units_duration(operators):
+    total_care_units_duration = dict()
+    for day_name, day in operators.items():
+        daily_total_care_units_duration = dict()
+        for care_unit_name, care_unit in day.items():
+            total_care_unit_duration = 0
+            for operator in care_unit.values():
+                total_care_unit_duration += operator['duration']
+            daily_total_care_units_duration[care_unit_name] = total_care_unit_duration
+        total_care_units_duration[day_name] = daily_total_care_units_duration
+    return total_care_units_duration
+
+def generate_care_unit_names():
+    care_unit_names = []
+    for care_unit_index in range(care_unit_number):
+        care_unit_names.append(f"cu{care_unit_index:02}")
+    return care_unit_names
+
+def generate_interdictions():
+    interdictions = dict()
+    for service_index in range(service_number):
+        service_interdictions = dict()
+        for other_service_index in range(service_number):
+            if random.random() >= interdiction_probability:
+                service_interdictions[f"srv{other_service_index:02}"] = 0
+                continue
+            service_interdictions[f"srv{other_service_index:02}"] = random.randint(interdiction_duration[0], interdiction_duration[1])
+        interdictions[f"srv{service_index:02}"] = service_interdictions
+    return interdictions
+
+def generate_necessities():
+    necessities = dict()
+    for service_index in range(service_number):
+        if random.random() >= necessity_probability:
+            necessities[f"srv{service_index:02}"] = dict()
+            continue
+        necessity_amount = random.randint(necessity_size[0], necessity_size[1])
+        service_indexes = random.sample(range(service_number), necessity_amount)
+        service_necessities = dict()
+        for other_service_index in sorted(service_indexes):
+            if other_service_index == service_index:
+                continue
+            start = random.randint(necessity_start[0], necessity_start[1])
+            duration = random.randint(necessity_duration[0], necessity_duration[1])
+            service_necessities[f"srv{other_service_index:02}"] = [
+                start,
+                start + duration
+            ]
+        necessities[f"srv{service_index:02}"] = service_necessities
+    return necessities
+
+def generate_patients():
+    patients = dict()
+    protocol_index = 0
+    for patient_index in range(patient_number):
+        patient = dict()
+        protocol_number = random.randint(patient_protocol[0], patient_protocol[1])
+        for _ in range(protocol_number):
+            protocol = dict()
+            iteration = []
+            packet_amount = random.randint(iteration_size[0], iteration_size[1])
+            packet_indexes = random.sample(range(packet_number), packet_amount)
+            for packet_index in sorted(packet_indexes):
+                existence_start = random.randint(protocol_start[0], protocol_start[1])
+                existence_duration = random.randint(protocol_duration[0], protocol_duration[1])
+                iteration.append({
+                    'packet_id': f"pkt{packet_index:02}",
+                    'start_date': random.randint(existence_start, existence_start + existence_duration),
+                    'freq': random.randint(protocol_frequency[0], protocol_frequency[1]),
+                    'since': "start_date",
+                    'tolerance': random.randint(protocol_tolerance[0], protocol_tolerance[1]),
+                    'existence': [
+                        existence_start,
+                        existence_start + existence_duration
+                    ]
+                })
+            iteration_amount = random.randint(iteration_number[0], iteration_number[1])
+            for interation_index in range(iteration_amount):
+                initial_shift = random.randint(iteration_shift[0], iteration_shift[1])
+                protocol[f"iter{interation_index:02}"] = [
+                    iteration,
+                    initial_shift
+                ]
+            patient[f"prot{protocol_index:02}"] = protocol
+        patient['priority_weight'] = random.randint(patient_priority[0], patient_priority[1])
+        patients[f"pat{patient_index:02}"] = patient
+    return patients
+
+def generate_full_input(operators, services, packets):
+            timestamp = generate_timestamp()
+            care_unit_names = generate_care_unit_names()
+            total_care_units_duration = generate_total_care_units_duration(operators)
+            interdictions = generate_interdictions()
+            necessities = generate_necessities()
+            patients = generate_patients()
+            full_input = {
+                'datecode' : timestamp,
+                'horizon': day_number,
+                'resources': care_unit_names,
+                'capacity' : total_care_units_duration,
+                'daily_capacity' : operators,
+                'services': services,
+                'interdiction': interdictions,
+                'necessity': necessities,
+                'abstract_packet': packets,
+                'pat_request': patients
+            }
+            return full_input
+
 if __name__ == "__main__":
 
     def check_number(n: str):
@@ -105,6 +237,7 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--number", metavar="NUM", type=check_number, default=1, help="number of instance to generate")
     parser.add_argument("-p", "--prefix", metavar="PRE", type=str, default="instance", help="prefix for the generated folder(s)")
     parser.add_argument("-o", "--output", metavar="OUT", type=str, default="instances", help="destination folder for the output")
+    parser.add_argument("--only-requests", action="store_true", help="don't generate the protocol input")
     parser.add_argument("-v", "--verbose", action="store_true", help="show what is done")
     args = parser.parse_args(sys.argv[1:])
 
@@ -122,16 +255,34 @@ if __name__ == "__main__":
             if args.verbose:
                 print(f"Created the folder '{args.prefix}{instance_index:02}'", end=" ")
         os.chdir(f"{args.prefix}{instance_index:02}")
+
+        services = generate_services()
+        packets = generate_packets()
+        operators = generate_operators()
+        priorities = generate_priorities()
+        if args.only_requests:
+            requests = generate_requests()
+        else:
+            full_input = generate_full_input(operators, services, packets)
+
         with open("services.json", "w") as f:
-            json.dump(generate_services(), f, indent=4)
+            json.dump(services, f, indent=4)
         with open("packets.json", "w") as f:
-            json.dump(generate_packets(), f, indent=4)
+            json.dump(packets, f, indent=4)
         with open("operators.json", "w") as f:
-            json.dump(generate_operators(), f, indent=4)
+            json.dump(operators, f, indent=4)
         with open("priorities.json", "w") as f:
-            json.dump(generate_priorities(), f, indent=4)
-        with open("requests.json", "w") as f:
-            json.dump(generate_requests(), f, indent=4)
+            json.dump(priorities, f, indent=4)
+        if args.only_requests:
+            with open("requests.json", "w") as f:
+                json.dump(requests, f, indent=4)
+            if os.path.isfile("full_input.json"):
+                os.remove("full_input.json")
+        else:
+            with open("full_input.json", "w") as f:
+                json.dump(full_input, f, indent=4)
+            if os.path.isfile("requests.json"):
+                os.remove("requests.json")
         if args.verbose:
             print(f"Created instance '{args.prefix}{instance_index:02}'")
         os.chdir("..")
